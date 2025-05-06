864e074af6a943b9c118c7ae21b848d27374e715
Abdulla Saleh <abdullasaleh@macbookair.myfiosgateway.com>
2025-05-06 19:51:50 -0400

    Fix

--- database/db.js
+++ database/db.js
@@ -6,23 +6,27 @@ It sets up Sequelize with Postgres database.
 ==================================================*/
 /* INSTANTIATE DATABASE */ 
 
+require('dotenv').config();
+
 // Import module dependencies
 const Sequelize = require('sequelize');  // Import Sequelize
 // const {dbName, dbUser, dbPwd} = require('./utils/configDB');  // Import database name, username, password
 
 // Display a confirmation message for opening a database connection
 console.log('Opening database connection');
-
+const isProduction = process.env.NODE_ENV === 'production';
 // This is the Sequelize entry point for connecting to the database. 
 const db = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
   protocol: 'postgres',
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false, // Required for Render's Postgres
-    },
-  },
+  dialectOptions: isProduction
+    ? {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false,
+        },
+      }
+    : {},
 });
 
 // Export Sequelize instance, which will be modified with models.
